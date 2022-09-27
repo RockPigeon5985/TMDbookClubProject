2486c5bdc4a5a0d37bc2c83f006825cc9199cb82
Varodi Andrei <Andrei-Constantin.Varodi@endava.com>
2022-09-27 14:32:00 +0300

    added go backend

--- GoLang Backend/go.mod
+++ GoLang Backend/go.mod
@@ -3,15 +3,19 @@ module GoLang_Backend
 go 1.19
 
 require (
-	Controllers v1.0.0
 	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
 )
 
 require (
-	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/lib/pq v1.1.1 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	gorm.io/datatypes v1.0.7 // indirect
+	gorm.io/driver/mysql v1.3.2 // indirect
+	gorm.io/gorm v1.23.6 // indirect
 )
-
-replace Controllers => ./Controllers
--- GoLang Backend/main.go
+++ GoLang Backend/main.go
@@ -1,33 +1,62 @@
 package main
 
 import (
-	"Controllers"
+	"GoLang_Backend/Controllers"
 	"GoLang_Backend/Database"
+	"GoLang_Backend/Security"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	"net/http"
 )
 
-var db = Database.DB
-
 func main() {
-	// API routes
+	// Start connection to database
+	Database.DBconnection()
+
+	// Router for endpoints
 	router := mux.NewRouter()
 
-	router.HandleFunc("/users", Controllers.GetUsers).Methods("GET")
-	router.HandleFunc("/user/{id}", Controllers.GetUser).Methods("GET")
-	router.HandleFunc("/user/create", Controllers.CreateUser).Methods("POST")
+	// User endpoints
+	router.Handle("/user/create", Security.ValidateJWT(Controllers.CreateUser)).Methods("POST")
 
+	// Book endpoints
+	router.Handle("/book/suggest", Security.ValidateJWT(Controllers.SuggestBook)).Methods("GET")
+
+	// BookList endpoints
+	router.Handle("/booklist/add", Security.ValidateJWT(Controllers.CreateBookList)).Methods("POST")
+	router.Handle("/booklist/available", Security.ValidateJWT(Controllers.AvailableForRent)).Methods("GET")
+	router.Handle("/booklist/borrowed", Security.ValidateJWT(Controllers.ShowBorrowedBooks)).Methods("GET")
+	router.Handle("/booklist/search", Security.ValidateJWT(Controllers.SearchBookByTitleOrAuthor)).Methods("GET")
+
+	// RentList endpoints
+	router.Handle("/rentlist/rent", Security.ValidateJWT(Controllers.CreateRent)).Methods("POST")
+	router.Handle("/rentlist/extend", Security.ValidateJWT(Controllers.ExtendRent)).Methods("POST")
+	router.Handle("/rentlist/rentedbooks", Security.ValidateJWT(Controllers.RentedBooks)).Methods("GET")
+
+	// WaitList endpoints
+	router.Handle("/waitlist/create", Security.ValidateJWT(Controllers.CreateWait)).Methods("POST")
+
+	// WishList endpoints
+	router.Handle("/wishlist/create", Security.ValidateJWT(Controllers.CreateWish)).Methods("POST")
+	router.Handle("/wishlist/show", Security.ValidateJWT(Controllers.ShowWishList)).Methods("GET")
+	router.Handle("/wishlist/delete", Security.ValidateJWT(Controllers.DeleteWish)).Methods("DELETE")
+
+	//JWT
+	router.HandleFunc("/jwt", Security.GetJwt)
+
+	// Start router
 	err := http.ListenAndServe("localhost:8080", router)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	// Close connection to database when router finish
-	defer func(db *gorm.DB) {
-		err := db.Close()
-		if err != nil {
-
+	// Close connection to database when exit main
+	defer func(DB *gorm.DB) {
+		DBerr := DB.Close()
+		if DBerr != nil {
+			fmt.Println("Database close connection error: " + DBerr.Error())
 		}
-	}(db)
+	}(Database.DB)
 }
